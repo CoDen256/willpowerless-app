2441c8ab7a3eb3d4cdfa82bb139c51591c3ebfc4
Denys Chernyshov <den.blackshov@gmail.com>
2024-11-08 14:25:21 +0100

    init

---
+++ app/src/main/java/io/github/coden/dictator/MainActivity.kt
@@ -0,0 +1,77 @@
+package io.github.coden.dictator
+
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.coden.dictator.ui.theme.DictatorTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var devicePolicyManager: DevicePolicyManager
+    private lateinit var adminComponent: ComponentName
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DictatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+
+        devicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        adminComponent = ComponentName(this, MyDeviceAdminReceiver::class.java)
+
+        // Check if the app is set as Device Owner
+        if (devicePolicyManager.isDeviceOwnerApp(packageName)) {
+            Toast.makeText(this, "App is a device owner, success", Toast.LENGTH_SHORT).show()
+            preventUninstall("com.reddit.frontpage")
+        } else {
+            Toast.makeText(this, "App is not Device Owner", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun preventUninstall(packageName: String) {
+        // Enable the uninstall block policy for the app
+//        devicePolicyManager.setUsbDataSignalingEnabled()
+//        devicePolicyManager.setApplicationRestrictions()
+        devicePolicyManager.setRecommendedGlobalProxy()
+        devicePolicyManager.setApplicationHidden(adminComponent, packageName, true)
+        devicePolicyManager.setUninstallBlocked(adminComponent, packageName, true)
+        Toast.makeText(this, "$packageName is protected from uninstallation", Toast.LENGTH_SHORT).show()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DictatorTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/io/github/coden/dictator/MyDeviceAdminReceiver.kt
@@ -0,0 +1,17 @@
+package io.github.coden.dictator
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyDeviceAdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+        // Code to execute when Device Admin is enabled
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
+        // Code to execute when Device Admin is disabled
+    }
+}
