1f51c0d47e2129dbe75d1f9d71bc911cdc08621b
Denys Chernyshov <den.blackshov@gmail.com>
2025-04-28 21:48:27 +0200

    better

--- app/src/main/java/io/github/coden256/wpl/guard/ui/MainScreen.kt
+++ app/src/main/java/io/github/coden256/wpl/guard/ui/MainScreen.kt
@@ -5,14 +5,18 @@ import android.app.Application
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.coden256.wpl.guard.ui.info.SystemInfoScreen
@@ -39,8 +43,8 @@ fun MainScreen(
 ) {
     Scaffold(
         topBar = {
-            Column {
-                TabRow(selectedTabIndex = mainViewModel.selectedTabIndex) {
+            Column(Modifier.padding(top = 15.dp)) {
+                SecondaryTabRow(selectedTabIndex = mainViewModel.selectedTabIndex) {
                     mainViewModel.tabs.forEachIndexed { index, tab ->
                         Tab(
                             selected = mainViewModel.selectedTabIndex == index,
