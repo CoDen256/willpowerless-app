808b2781aaca0556879ab2e396492bcc7675483a
Denys Chernyshov <den.blackshov@gmail.com>
2025-04-29 11:45:15 +0200

    export to provide waking up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.compose.material3.material3)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.core.ktx)
+    implementation(libs.autostarter)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
